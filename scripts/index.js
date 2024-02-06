fd7d9b0131232cbd746d49218f7afcf79be8b661
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-02-05 20:55:38 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function createLog(txt, clr) {
-        var newLog = document.createElement("h6");
-        newLog.innerHTML = txt;
-        newLog.style.color = clr
-        document.body.appendChild(newLog)
-    };
-    createLog('Startup Initized', 'white');
-    //check url protocol (for those dumbasses who run from html or dev)
-    var runSW = "true";
-    if (!location.href.includes("https://")) {
-      createLog("Services Workers can't be registered with this protocol", 'red');
-      runSW = "false";
-    }
-
-    var SW_Status
-    // check for register sw
-    if (runSW !== "false") {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.getRegistration().then(function(registration) {
-          if (registration) {
-            createLog("SW found. Running...")
-            SW_Status = "on"
-          } else {
-            createLog("no SW found. loading with built-in")
-            SW_Status = "non"
-          }
-        }).catch(function(error) {
-          createLog("no SW found. loading with built-in", 'yellow')
-          SW_Status = "non"
-        });
-      } else {
-        alert('Service workers are not supported in this browser.');
-        return
-      }
-    }
-    sessionStorage.setItem("swRun", runSW)
-    // start page
-    setTimeout(() => {
       let inFrame
 
       try {
@@ -72,6 +34,4 @@ document.addEventListener("DOMContentLoaded", function () {
           location.replace(localStorage.getItem('panicLink') || 'https://www.nasa.gov/')
         }
       }
-    }, 1000);
-      
 });
